c3d509024816ffc8725be35f598584b89afb0854
Otech2018 <ajahogonnaya2012@gmail.com>
2020-12-04 16:57:52 +0100

    Admin Wrking with everything in Writersgig as of then

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,11 +8,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>{{config('app.name', 'Solomonsideas)}}</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>{{config('app.name', 'Solomonsideas')}}</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
   
  
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
    <!-- Custom styles for this template -->
   <link href="{{ asset('css/simple-sidebar.css')}}" rel="stylesheet">
   <link href="{{ asset('css/side_bar.css')}}" rel="stylesheet">
@@ -26,12 +26,11 @@
  @yield('content')
   
 
- 
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" ></script>
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" ></script>
- <script src="{{ asset('js/index.js') }}"></script>  
-
+ 
 
  <script src="{{ asset('dropify/dist/js/dropify.min.js') }}"></script>
  
