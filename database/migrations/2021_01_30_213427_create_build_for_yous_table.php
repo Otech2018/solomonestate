27cae5ad1a5e9e2b06d4e64794018c1cf9874ec3
Otech2018 <ajahogonnaya2012@gmail.com>
2021-01-30 22:45:44 +0100

    gf

--- database/migrations/2021_01_30_213427_create_build_for_yous_table.php
+++ database/migrations/2021_01_30_213427_create_build_for_yous_table.php
@@ -15,6 +15,21 @@ class CreateBuildForYousTable extends Migration
     {
         Schema::create('build_for_yous', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('budget_amount')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone1')->nullable();
+            $table->string('phone2')->nullable();
+            $table->string('country')->nullable();
+            $table->string('state')->nullable();
+            $table->string('state1')->nullable();
+            $table->string('lga')->nullable();
+            $table->string('lga1')->nullable();
+            $table->string('location_address')->nullable();
+            $table->string('status')->nullable();
+
             $table->timestamps();
         });
     }
@@ -27,5 +42,5 @@ class CreateBuildForYousTable extends Migration
     public function down()
     {
         Schema::dropIfExists('build_for_yous');
-    }
+    } 
 }
